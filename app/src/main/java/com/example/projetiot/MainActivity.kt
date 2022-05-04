b416bbc00958568a225d3126de96a876591beac9
Emilien NURY <emilien.nury@etu.univ-lyon1.fr>
2022-05-04 22:34:44 +0200

    Added refresh button

--- app/src/main/java/com/example/projetiot/MainActivity.kt
+++ app/src/main/java/com/example/projetiot/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetiot.adapter.AdapterFirebase
@@ -14,18 +15,28 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 private lateinit var database: DatabaseReference
+private lateinit var recyclerView: RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView = this.findViewById<RecyclerView>(R.id.firebaseRecylerView)
+        val refreshBtn = this.findViewById<Button>(R.id.btn_refresh)
+        refreshBtn.setOnClickListener {
+            getData()
+        }
         database = Firebase.database.reference
+        getData()
+
+    }
+    fun getData() {
         database.child("Alarm").get().addOnSuccessListener {
             var alarm: HashMap<String,String> = it.value as HashMap<String, String>;
             database.child("Entry").get().addOnSuccessListener {
                 var entry: HashMap<String,String> = it.value as HashMap<String, String>;
-                val recyclerView = this.findViewById<RecyclerView>(R.id.firebaseRecylerView)
                 var result = HashMap<String, Data>()
                 for (a in alarm) {
                     result.put(a.key, Data(a.value, "alarm"))
@@ -44,6 +55,5 @@ class MainActivity : AppCompatActivity() {
         }.addOnFailureListener{
             Log.e("firebase", "Error getting data", it)
         }
-
     }
 }
